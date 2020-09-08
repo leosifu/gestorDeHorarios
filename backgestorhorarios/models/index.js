9af11c1715500f34f7fd53403dbd31d1e1e5fcab
Leo <leo.sifuentes.13@gmail.com>
2020-09-08 20:46:39 -0300

    arreglo3

--- backgestorhorarios/models/index.js
+++ backgestorhorarios/models/index.js
@@ -18,14 +18,15 @@ const db = {};
 const sequelize = new Sequelize(process.env.DB_SCHEMA || 'scheduleManager',
                                 process.env.DB_USER || 'root',
                                 process.env.DB_PASSWORD || '123456',
-                                {
-                                    host: process.env.DB_HOST || 'localhost',
-                                    port: process.env.DB_PORT || 3306,
-                                    dialect: 'mysql',
-                                    dialectOptions: {
-                                        socketPath: "/var/run/mysqld/mysqld.sock"
-                                    },
-                                });
+                                  {
+                                      host: process.env.DB_HOST || 'localhost',
+                                      port: process.env.DB_PORT || 3306,
+                                      dialect: 'mysql',
+                                      // dialectOptions: {
+                                      //     socketPath: "/var/run/mysqld/mysqld.sock"
+                                      // },
+                                  }
+                                );
 
 fs
   .readdirSync(__dirname)
