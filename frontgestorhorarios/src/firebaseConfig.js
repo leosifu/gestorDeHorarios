bb06d68f07404926c9fe85a9a65250156b973f8b
Leonardo <leonardo.sifuentes@usach.cl>
2021-08-29 17:45:02 -0400

    fix

--- frontgestorhorarios/src/firebaseConfig.js
+++ frontgestorhorarios/src/firebaseConfig.js
@@ -1,11 +1,11 @@
 const config = {
-  apiKey: "AIzaSyBuFCuo60RHf6UF1_eB9-br1M1CeimaSVU",
-  authDomain: "gestorhorarios-d0ffe.firebaseapp.com",
-  databaseURL: "https://gestorhorarios-d0ffe.firebaseio.com",
-  projectId: "gestorhorarios-d0ffe",
-  storageBucket: "gestorhorarios-d0ffe.appspot.com",
-  messagingSenderId: "213185244694",
-  appId: "1:213185244694:web:c2502fb5e5eba1600a0f37",
-  measurementId: "G-EZ2STQ8GX1"
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASEURL,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+  measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 export default config;
