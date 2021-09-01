7b1c517f99edef2c4aa8ff932dc0cde7248e4015
root <root@naboo.diinf.usach.cl>
2021-09-01 09:38:56 -0400

    going to production

--- frontgestorhorarios/src/config/urls.js
+++ frontgestorhorarios/src/config/urls.js
@@ -1,6 +1,6 @@
 
 
-const baseUrl = window.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
+const baseUrl = process.env.REACT_APP_API_ENDPOINT || 'http://localhost:8000';
 // const baseUrl = 'https://shielded-basin-33684.herokuapp.com/'
 
 export default baseUrl;
